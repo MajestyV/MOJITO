d3ba735e9457d7655a5767867b91f502e2fa3eb8
MajestyV <53797732+MajestyV@users.noreply.github.com>
2021-04-22 21:30:45 +0800

    Add files via upload

---
+++ MOJITO/AnaData.py
@@ -0,0 +1,126 @@
+import numpy as np
+# import scipy as sp
+import matplotlib.pyplot as plt
+from scipy.optimize import leastsq
+from MOJITO import GetSpectra
+
+pi = np.pi
+def ln(x): return np.log(x)
+def exp(x): return np.exp(x)
+
+class anda:
+    """ This class of functions is designed to help analysis the spectra. """
+    #  为了单位尺度的统一，在这个函数包中，我们一律采用角频率w作为自变量以方便使用de Broglie relations进行角频率-能量转换(E=hbar*w)
+
+    def __init__(self):
+        self.name = anda
+
+    # 采用了跟粒子激发态寿命导致的自然致宽相关的洛伦兹分布：L(v) = (1/pi)*gamma/[4*pi**2*(v-v0)**2+(gamma/w)**2], w = 2*pi*v, tau = 1/gamma
+    def Lorentzaian(self,w,param):
+        w0, gamma, intensity = param  # w0是峰位，gamma是半峰全宽（FWHM），tau = 1/gamma是寿命
+        return intensity*(gamma/2.0)/(pi*((w-w0)**2+(gamma/2.0)**2))
+
+    # 跟粒子碰撞相关的多普勒致宽的高斯分布：G(v) = np.sqrt(ln(2)/pi)*exp(-(v-v0)**2/(2*sigma**2))/sigma, sigma是高斯分布的方差
+    def Gaussian(self,w,param):
+        w0, alpha, intensity = param  # w0是峰位，alpha是半峰全宽（FWHM），高斯分布的方差sigma = alpha/2*np.sqrt(2*ln(2))
+        return np.array([intensity*np.sqrt(ln(2)/pi)*(2.0/alpha)*exp(-(2*pi*w[i]-2*pi*w0)**2*ln(2)*(2.0/alpha)**2) for i in range(len(w))])
+
+    # Voigt函数是高斯分布跟洛伦兹分布的卷积，这条函数假设两种组分有着同样的峰位
+    def Voigt(self,w,param):
+        w0, gamma, alpha, intensity = param
+        Lw = self.Lorentzaian(w,[w0,gamma])
+        Gw = self.Gaussian(w,[w0,alpha])
+        return np.convolve(Lw,Gw,'same')
+
+    def function(self,function_name):
+        function_dict = {'Lorentzian':self.Lorentzaian,
+                         'Gaussian':self.Gaussian,
+                         'Voigt':self.Voigt}
+        nparam_dict = {'Lorentzian':3,
+                       'Gaussian':3,
+                       'Voigt':4}
+        return function_dict[function_name],nparam_dict[function_name]
+
+    # 背景基线函数
+    def Baseline(self,x,baseline): return baseline
+
+    def hypothesis_func(self,param_set,x,num_peaks,num_param,mode):
+        func = 180
+        param_list = np.copy(param_set)
+        param = np.zeros((num_param), dtype=float)  # num_param会比调用标准波形函数的参数个数多1个，因为最后一位是强度参数，而标准函数的强度固定，不考虑强度变化
+        for i in range(num_peaks):
+            for j in range(num_param):
+                param[j] = param_list[i*num_param+j+1]
+            func += self.function(mode)[0](x,param)
+            # print(param)
+        return func
+
+    def Error(self,param_set,x,y,num_peaks,num_param,mode):
+        return y-self.hypothesis_func(param_set,x,num_peaks,num_param,mode)
+
+    # 计算拟合用的函数与测试结果的误差
+    # def Error(self,x,y,num_peaks):
+        # return
+
+    # 需拟合的原始数据的输入形式应为：[[x0, x1, x2......], [y0, y1, y2......]]
+    def Fitting(self,Datum,**kwargs):
+        x, y = Datum
+
+        if 'num_peaks' in kwargs:
+            npeaks_range = [kwargs['num_peaks']]
+        else:
+            if 'num_peaks search range' in kwargs:
+                npeaks_range = list(range(kwargs['num_peaks search range']))
+            else:
+                npeaks_range = list(range(0,10))
+
+        mode = kwargs['mode'] if 'mode' in kwargs else 'Voigt'  # 决定用于拟合的函数线型
+        num_param = self.function(mode)[1]  # 单个波形函数的参数个数
+        baseline = kwargs['baseline'] if 'baseline' in kwargs else 0  # 函数的基线，用于数据在y轴方向有平移的情况
+
+        for n in range(len(npeaks_range)):
+            num_peaks = npeaks_range[n]
+            #print(num_peaks)
+            # 输入应为[[函数1的参数1, 函数1的参数2], [函数2的参数1, 函数2的参数2], [函数3的参数1, 函数3的参数2]......]
+            # nparam+1的1为强度参数，Intensity（因为所有预设函数都是分布函数的形式，积分为1）
+            initial_param = kwargs['param'] if 'param' in kwargs else [[1]*(num_param)]*num_peaks
+            initial_param = [initial_param[i][j] for i in range(num_peaks) for j in range(num_param)]
+            # 把出入参数解压成[函数1的参数1, 函数1的参数2, 函数2的参数1, 函数2的参数2, 函数3的参数1, 函数3的参数2......]形式方便拟合
+            initial_param = np.array([baseline]+initial_param)  # 在最开头加上基线，并将数据类型转化为数组
+
+            #print(initial_param[6])
+
+            fitted_param = leastsq(self.Error,initial_param,args=(x,y,num_peaks,num_param,mode))
+
+        return fitted_param
+
+if __name__ == '__main__':
+    gd = GetSpectra.geda()
+    ad = anda()
+
+    data_directory = "D:/OneDrive/OneDrive - The Chinese University of Hong Kong/Desktop/Testing/GetData/B1-Raman.txt"
+    data = gd.Extract(data_directory, output_mode='original data')
+    data = gd.Range(data,340,450)
+    data = gd.Rearrange(data)
+
+    # print(gd.Extract(data_directory,output_mode='data'))
+    # print(gd.Extract(data_directory,output_mode='original data'))
+    # print(gd.Extract(data_directory,output_mode='testing parameters'))
+    # print(gd.Range(data,350,450))
+    #print(data)
+    #print(gd.Visualize(data, xlim=(350, 450), ylim=(150, 250)))
+    # print(ad.Fitting(data,num_peaks=2,param=[[380,1,1],[410,1,1]],mode='Lorentzian'))
+
+    Para = ad.Fitting(data,num_peaks=2,param=[[380,10,1],[410,10,1]],mode='Lorentzian')[0]
+    print(Para)
+    def FittedCurve(x):
+        baseline, w1, gamma1, i1, w2, gamma2, i2= Para
+        f1 = i1*(gamma1/2)/(pi*((x-w1)**2+(gamma1/2)**2))
+        f2 = i2 * (gamma2 / 2) / (pi * ((x - w2) ** 2 + (gamma2 / 2) ** 2))
+        # f3 = i3 * (gamma3 / 2) / (pi * ((x - w3) ** 2 + (gamma3 / 2) ** 2))
+        return baseline+f1+f2
+    x0 = np.linspace(340,450,100)
+
+    plt.plot(x0,FittedCurve(x0))
+    plt.plot(data[0],data[1])
+
--- MOJITO/GetSpectra.py
+++ MOJITO/GetSpectra.py
@@ -9,17 +9,18 @@ class geda:
     def __init__(self):
         self.name = geda
 
+    # The word "Datum" (with capital "D") indicates the data list of one curves, and "Data" is data lists of multiple curves.
     # 抽取某一段范围(x_start, x_end)内的数据，输入的数据列表形式应为[[x0,y0],[x1,y1],[x2,y2]......]
-    def Range(self,DataList,x_start,x_end):
+    def Range(self,Datum,x_start,x_end):
         sampled_data = []
-        for n in range(len(DataList)):
-            if DataList[n][0] >= x_start and DataList[n][0] <= x_end:
-                sampled_data.append(DataList[n])
+        for n in range(len(Datum)):
+            if Datum[n][0] >= x_start and Datum[n][0] <= x_end:
+                sampled_data.append(Datum[n])
         return sampled_data
 
     # 通过逐行读取txt文件中的信息得到的数据形式为[[x0,y0],[x1,y1],[x2,y2]......]，此函数可以将其重排为[[x0,x1,x2......],[y0,y1,y2......]]
-    def Rearrange(self,DataList):
-        return [[DataList[i][0] for i in range(len(DataList))],[DataList[j][1] for j in range(len(DataList))]]
+    def Rearrange(self,Datum):
+        return [[Datum[i][0] for i in range(len(Datum))],[Datum[j][1] for j in range(len(Datum))]]
 
     def Extract(self,DataFile, title_pattern="#", output_mode='data'):
         testing_parameters = []
@@ -43,19 +44,32 @@ class geda:
         return result[output_mode]
 
     # Visualize module
-    # def ColorfulCurve(self):
+    def Color(self,num_curves):  # 颜色序列
+        color_dict = {'line_1':'#800080','line_2':'#8000ff','line_3':'#8080ff','line_4':'#00407f','line_5':'#0000ff',
+                      'line_6':'#0080ff','line_7':'#00cbff','line_8':'#00c957','line_9':'#fad925','line_10':'#fe7f00',
+                      'line_11':'#ff0080','line_12':'#ff0000'}
+        color_sequence = list(color_dict.keys())
+        return [color_dict[color_sequence[n%len(color_sequence)]] for n in range(num_curves)]
+
+    def Seperate(self,Data,step):
+        for n in range(len(Data)):
+            shift = n*step
+            for m in range(len(Data[n][1])):
+                Data[n][1][m] = Data[n][1][m]+shift
+        return Data
 
     # 输入形式为：Data = [测试数据列表1，测试数据列表2，测试数据列表3......]
     def Visualize(self,Data,**kwargs):
         if not isinstance(Data[0][0],list):  # 对于单个测试的数据，如果没有写成[测试数据列表]这样的形式，则对其进行格式修正
             Data = [Data]
+        num_curves = len(Data)  # number of curves in the plot
 
-        curve_label = kwargs['curve label'] if 'curve label' in kwargs else [None]*len(Data)  # 默认设定不对曲线进行标注
+        curve_label = kwargs['curve label'] if 'curve label' in kwargs else [None]*num_curves  # 默认设定不对曲线进行标注
         linewidth = kwargs['linewidth'] if 'linewidth' in kwargs else 1.0      # 曲线线宽
         linestyle = kwargs['linestyle'] if 'linestyle' in kwargs else '-'      # 曲线线型
-        marker = kwargs['marker'] if 'marker' in kwargs else [None]*len(Data)  # 默认设定不显示数据点
+        marker = kwargs['marker'] if 'marker' in kwargs else [None]*num_curves  # 默认设定不显示数据点
         markersize = kwargs['markersize'] if 'markersize' in kwargs else 5.0   # 控制数据点的大小
-        color = kwargs['color'] if 'color' in kwargs else ['#800080','#8000ff','#8080ff','#00407f','#0000ff','#0080ff','#00cbff','#00c957','#fad925','#fe7f00','#ff0080','#ff0000']  # 曲线的颜色
+        color = kwargs['color'] if 'color' in kwargs else self.Color(num_curves)  # 曲线的颜色
         xlabel = kwargs['xlabel'] if 'xlabel' in kwargs else 'X'
         ylabel = kwargs['ylabel'] if 'ylabel' in kwargs else 'Y'
         fontsize = kwargs['fontsize'] if 'fontsize' in kwargs else 16
@@ -63,10 +77,13 @@ class geda:
         xlim = kwargs['xlim'] if 'xlim' in kwargs else (min(Data[0][0]),max(Data[0][0]))
         ylim = kwargs['ylim'] if 'ylim' in kwargs else (min(Data[0][1]),max(Data[0][1]))
 
+        step = kwargs['step'] if kwargs['Seperated'] == 'True' else 0
+        Data = self.Seperate(Data,step)
+
         plt.rcParams['xtick.direction'] = 'in'
         plt.rcParams['ytick.direction'] = 'in'
 
-        for n in range(len(Data)):
+        for n in range(num_curves):
             x, y = Data[n]
             plt.plot(x,y,label=curve_label[n],linewidth=linewidth,linestyle=linestyle,color=color[n],marker=marker[n],markersize=markersize)
 
@@ -80,7 +97,7 @@ class geda:
 
 if __name__ == '__main__':
     gd = geda()
-    data_directory = "D:/OneDrive/OneDrive - The Chinese University of Hong Kong/Desktop/Testing/GetData/B1-Raman.txt"
+    data_directory = "D:/OneDrive/OneDrive - The Chinese University of Hong Kong/Desktop/Testing/GetData/D1-Raman.txt"
     data = gd.Extract(data_directory,output_mode='data')
     #print(gd.Extract(data_directory,output_mode='data'))
     #print(gd.Extract(data_directory,output_mode='original data'))
